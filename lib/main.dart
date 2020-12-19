0c5d2c60541429edaa13818c8fec79a5860a55ae
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-12-19 19:38:26 -0300

    Component answer and question

--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,47 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import './question.dart';
+import './answer.dart';
 
-void main() {
-  runApp(MyApp());
-}
+main() => runApp(new QuestionsApp());
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+class _QuestionsAppState extends State<QuestionsApp> {
+  var _selectedQuestion = 0;
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _answer() {
     setState(() {
-      _counter++;
+      _selectedQuestion++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+    final List<String> questions = [
+      'Qual a sua linguagem de programação favorita?',
+      'Qual a sua IDE favorita?'
+    ];
+
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+        ),
+        body: Column(
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Question(questions.elementAt(_selectedQuestion)),
+            Answer('Resposta 1', _answer),
+            Answer('Resposta 1', _answer),
+            Answer('Resposta 1', _answer)
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
     );
+  }
+}
+
+class QuestionsApp extends StatefulWidget {
+  @override
+  _QuestionsAppState createState() {
+    return _QuestionsAppState();
   }
 }
